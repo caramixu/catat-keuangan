ead12f817e32d6bd0547259435f804976493e11d
Fadhil Muzakki N <necroambulatory1994@gmail.com>
2026-05-11 19:19:11 +0700

    add icon title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="icon" type="image/png" href="{{ asset('images/omj-logo.png') }}">
     <title>{{ config('app.name', 'OMJ - Catat Keuangan') }}</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
